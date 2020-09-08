e771f5557d3aae4a023ef007dbbb342fa977e575
sonptt <sonptt@vng.com.vn>
2020-09-08 11:41:46 +0700

    Change to GridLayoutManager to display more stickers

--- app/src/main/java/com/example/rlottiebenchmark/MainActivity.java
+++ app/src/main/java/com/example/rlottiebenchmark/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.rlottiebenchmark;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -11,13 +12,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int STICKER_SIZE = 240;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         StickerAdapter adapter = new StickerAdapter();
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        int spanCount = getScreenWidth() / AndroidUtilities.dp(STICKER_SIZE);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, spanCount);
         RecyclerView rv = findViewById(R.id.rv);
         rv.setLayoutManager(layoutManager);
         rv.setAdapter(adapter);
@@ -29,8 +33,14 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 429; i++) {
             String stickerName = "sticker_" + (i + 1);
             int resId = getResources().getIdentifier(stickerName, "raw", getPackageName());
-            result.add(new StickerAdapter.StickerUiModel(resId, 600, 600));
+            result.add(new StickerAdapter.StickerUiModel(resId, STICKER_SIZE, STICKER_SIZE));
         }
         return result;
+    }
+
+    private int getScreenWidth() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
     }
 }
