cad490ca3d3a992f3ab0a0fddfabb16c933d382d
sonptt <sonptt@vng.com.vn>
2020-09-18 18:14:57 +0700

    Caching stickers to optimize performance

--- app/src/main/java/com/example/rlottiebenchmark/MainActivity.java
+++ app/src/main/java/com/example/rlottiebenchmark/MainActivity.java
@@ -1,26 +1,62 @@
 package com.example.rlottiebenchmark;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int STICKER_SIZE = 240;
+    private int mStickerSize = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            onPermissionsGranted();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != 100 || grantResults.length == 0) {
+            return;
+        }
+
+        for (int i = 0; i < grantResults.length; i++) {
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        onPermissionsGranted();
+    }
+
+    private void onPermissionsGranted() {
+        mStickerSize = getScreenWidth() / 5;
 
         StickerAdapter adapter = new StickerAdapter();
-        int spanCount = getScreenWidth() / AndroidUtilities.dp(STICKER_SIZE);
+        int spanCount = 5;
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, spanCount);
         RecyclerView rv = findViewById(R.id.rv);
         rv.setLayoutManager(layoutManager);
@@ -30,10 +66,11 @@ public class MainActivity extends AppCompatActivity {
 
     private List<StickerAdapter.StickerUiModel> buildStickers() {
         final List<StickerAdapter.StickerUiModel> result = new ArrayList<>();
-        for (int i = 0; i < 429; i++) {
+        for (int i = 0; i < 100; i++) {
             String stickerName = "sticker_" + (i + 1);
             int resId = getResources().getIdentifier(stickerName, "raw", getPackageName());
-            result.add(new StickerAdapter.StickerUiModel(resId, STICKER_SIZE, STICKER_SIZE));
+            copyFiletoCache(resId, stickerName + ".zls");
+            result.add(new StickerAdapter.StickerUiModel(getCacheDir() + "/" + stickerName + ".zls", mStickerSize, mStickerSize));
         }
         return result;
     }
@@ -42,5 +79,29 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         return displayMetrics.widthPixels;
+    }
+
+    private void copyFiletoCache(int resourceId, String resourceName) {
+        String cacheDir = getCacheDir() + "/" + resourceName;
+        try {
+            InputStream in = getResources().openRawResource(resourceId);
+            FileOutputStream out;
+            out = new FileOutputStream(cacheDir);
+            byte[] buff = new byte[1024];
+            int read;
+            try {
+                while ((read = in.read(buff)) > 0) {
+                    out.write(buff, 0, read);
+                }
+            } finally {
+                in.close();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/example/rlottiebenchmark/StickerAdapter.java
+++ app/src/main/java/com/example/rlottiebenchmark/StickerAdapter.java
@@ -3,16 +3,15 @@ package com.example.rlottiebenchmark;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rlottiebenchmark.widget.RLottieDrawable;
 import com.example.rlottiebenchmark.widget.RLottieImageView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -21,7 +20,7 @@ class StickerAdapter extends RecyclerView.Adapter<StickerAdapter.StickerHolder> 
 
     private List<StickerUiModel> stickers = new ArrayList<>();
 
-    private ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+    private ExecutorService mExecutor = Executors.newFixedThreadPool(5);
 
     @NonNull
     @Override
@@ -58,13 +57,12 @@ class StickerAdapter extends RecyclerView.Adapter<StickerAdapter.StickerHolder> 
         }
 
         public void bindView(final StickerUiModel stickerUiModel, ExecutorService executor) {
-            mImgView.setAnimation(-1, stickerUiModel.width, stickerUiModel.height);
             mImgView.invalidate();
             if (currentTask != null && (!currentTask.isCancelled() || !currentTask.isDone())) {
                 currentTask.cancel(true);
             }
             currentTask = executor.submit(() -> {
-                RLottieDrawable drawable = new RLottieDrawable(stickerUiModel.resId, "" + stickerUiModel.resId, AndroidUtilities.dp(stickerUiModel.width), AndroidUtilities.dp(stickerUiModel.height), false, null);
+                RLottieDrawable drawable = new RLottieDrawable(new File(stickerUiModel.path), AndroidUtilities.dp(stickerUiModel.width), AndroidUtilities.dp(stickerUiModel.height), true, false);
                 AndroidUtilities.runOnUIThread(() -> {
                     mImgView.setAutoRepeat(true);
                     mImgView.setAnimation(drawable);
@@ -76,15 +74,14 @@ class StickerAdapter extends RecyclerView.Adapter<StickerAdapter.StickerHolder> 
     }
 
     public static class StickerUiModel {
-        @IdRes
-        private final int resId;
+        private final String path;
 
         private final int width;
 
         private final int height;
 
-        public StickerUiModel(int resId, int width, int height) {
-            this.resId = resId;
+        public StickerUiModel(String path, int width, int height) {
+            this.path = path;
             this.width = width;
             this.height = height;
         }
